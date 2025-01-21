c0a2146077489f10a106b5c6d25ffcea1ebdd86d
CheolHyun Park <43946503+IRONAGE-Park@users.noreply.github.com>
2025-01-21 09:32:58 +0900

    `Tooltip`의 화살표를 표시하는 선택자 방식을 변경하고, 기본 텍스트 컬러를 반영합니다. (#10)
    * FIX: Tooltip default color and Arrow positioning by `ref` [complete]
    
    * UPDATE: version `v0.2.9` [complete]

--- src/components/Tooltips/styles/Tooltip.style.ts
+++ src/components/Tooltips/styles/Tooltip.style.ts
@@ -4,16 +4,16 @@ import type { TextTooltipProps } from "@components/Tooltips/TextTooltip";
 import styled, { css } from "@themes/styled";
 // styles
 
-// eslint-disable-next-line import/prefer-default-export
 export const StyleTooltipText = styled.p<Pick<TextTooltipProps, "textSize">>`
-  color: #fff;
   ${({ theme }) => theme.font.kopub.contents5}
+
+  line-height: 1.2;
 
   ${({ textSize }) =>
     textSize &&
     css`
       font-size: ${textSize}px;
     `}
-  
+
   margin: 0;
 `;
